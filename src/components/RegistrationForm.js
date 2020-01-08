f5c20c476760233e0782306aae4cb88ad4e85173
ThomasCluzel <thomas.cluzel@etu.uca.fr>
2020-01-08 17:28:56 +0100

    :construction: RegistrationForm with post with API.js

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,16 +1,22 @@
 // Component to register a new user
 
 import React from 'react';
+import API from '../API';
 // TODO: import style and add className="RegistrationFrom" to the global div
-
-const api_url = "localhost:5000/api/v1/";
+// Add some text to display if the creation succeeded or failed
 
 const RegistrationForm = () => (
     <div>
-        <form action={api_url + "users"} method="post">
-            Name: <input type="text" name="name" /> <br />
-            Pseudo: <input type="text" name="pseudo" /> <br />
-            Password: <input type="password" name="password" /> <br />
+        <form onSubmit={ () => {
+            API.post('users', { name: this.refs.name, pseudo: this.refs.pseudo })
+            .then(
+                ok => {}, // TODO: display a message with the Snackbar of Material UI
+                err => {} // TODO: display an error
+            )
+        }}>
+            Name: <input type="text" placeholder="John Smith" name="name" ref="name" /> <br />
+            Pseudo: <input type="text" placeholder="xXDarkRaptorKillerXx" name="pseudo" ref="pseudo" /> <br />
+            Password: <input type="password" name="password" ref="password" /> <br />
             <input type="submit" value="Register" />
         </form>
     </div>
