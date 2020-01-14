c5d9e8f6ef751f52b64698539807d6a4bf2baf6f
ThomasCluzel <thomas.cluzel@etu.uca.fr>
2020-01-14 13:10:39 +0100

    :ambulance: useState does not what I thought

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -9,21 +9,23 @@ import theme from '../styles/theme';
 
 const RegistrationForm = () => {
     // state
-    const [name, setName] = useState(0);
-    const [pseudo, setPseudo] = useState(1);
-    const [password, setPassword] = useState(2);
-    const [confirmPassword, setConfirmPassword] = useState(3);
+    const [name, setName] = useState('');
+    const [pseudo, setPseudo] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        API.post('users', { name: name, pseudo: pseudo })
+        .then(
+            ok => { console.log('ok'); }, // TODO: display a message with the Snackbar of Material UI
+            err => { console.log('ok'); } // TODO: display an error
+        )
+    }
 
     return (
         <ThemeProvider theme={theme} >
-            <form onSubmit={ (e) => {
-                e.preventDefault();
-                API.post('users', { name: name, pseudo: pseudo })
-                .then(
-                    ok => {}, // TODO: display a message with the Snackbar of Material UI
-                    err => {} // TODO: display an error
-                )
-            }}>
+            <form onSubmit={submitForm}>
                 <TextField id="name" variant={theme.props.variant} label="Name" required onChange={(e)=>{setName(e.target.value);}} />  <br />
                 <TextField id="pseudo" variant={theme.props.variant} label="Pseudo" requiredonChange={(e)=>{setPseudo(e.target.value);}} />  <br />
                 <TextField id="password" variant={theme.props.variant} label="Password" type="password" required onChange={(e)=>{setPassword(e.target.value);}} />  <br />
