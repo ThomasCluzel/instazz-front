79246c1fe193cc7566ebd8d1dc16f498c072e224
ThomasCluzel <thomas.cluzel@etu.uca.fr>
2020-01-30 10:01:17 +0100

    :lipstick: Enhanced posts UI

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Container } from '@material-ui/core';
 
 /**
  * TODOs:
- * - Better UI
+ * - Why not a link on the author of the post to visit his/her profile
  */
 
 // NavBar style
 const useStyle = makeStyles(theme => ({
     post: {
-        textAlign: "center",
-        minWidth: "40vw",
-        maxWidth: "70vw"
+        textAlign: "left",
+        border: "1pt solid " + theme.palette.primary.main,
+        borderRadius: "10px",
+        margin: "4px",
+        padding: "8px"
     },
     author: {
-        textAlign: "right"
+        fontSize: "large"
     }
 }));
 
@@ -22,15 +24,15 @@ const PostListItem = ({post}) => {
     const classes = useStyle();
 
     return (
-        <div className={classes.post}>
-            <img src={post.image.filename} alt="" width="100%" />
+        <Container maxWidth="md" className={classes.post}>
+            <p className={classes.author}>
+                Posted by {post.author.pseudo}
+            </p>
+            <img src={post.image.filename} alt={"posted by " + post.author.pseudo} width="100%" />
             <p>
                 {post.description}
             </p>
-            <p className={classes.author}>
-                {post.author.pseudo}
-            </p>
-        </div>
+        </Container>
     );
 };
 
