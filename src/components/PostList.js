50730021acba51a9b4312aac0fee6e9daa80d199
ThomasCluzel <thomas.cluzel@etu.uca.fr>
2020-02-10 19:06:04 +0100

    :sparkles: Append more posts at the end of the list when the user reach the bottom

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { List, Snackbar, CircularProgress } from '@material-ui/core';
+import { List, Snackbar, CircularProgress, makeStyles } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import PostListItem from './PostListItem';
 import API from '../API';
 
 /**
  * TODOs:
- * - Each time the user reach the bottom of the page, load more posts (to keep in on our web site ahahah)
- * - Better and more beautiful error message
  * - Maybe a props to know if we need to fetch
  *   - all the recent posts
  *   - only the user's posts
@@ -17,6 +15,14 @@ import API from '../API';
 // Constants
 const NUMBER_OF_INITIAL_POST = 10; // load 10 posts at the beginning
 const NUMBER_OF_POST_TO_LOAD = NUMBER_OF_INITIAL_POST / 2; // each time the user reach the end
+
+// Style
+const useStyles = makeStyles({
+    center: {
+        display: "flex",
+        justifyContent: "center"
+    }
+});
 
 /*
 let onePost = {
@@ -39,13 +45,22 @@ for(let i = 0; i < 3; i++)
 }
 */
 
+/**
+ * Display a list of posts
+ * 
+ * @param {*} props is 
+ */
 const PostList = () => {
+    // style
+    const classes = useStyles();
+
     // state
     const [ isLoading, setIsLoading ] = useState(true); // when the user comes to the page, we load the posts
     const [ errorFromServer, setErrorFromServer ] = useState(false);
     const [ postList, setPostList ] = useState([]);
-    const [ alertShown, setAlertShown ] = React.useState(false);
-    const [ errorMsg, setErrorMsg ] = React.useState('');
+    const [ currentPageOfPosts, setCurrentPageOfPosts ] = useState(2); // start at 2 because we load 2 pages at the beginning
+    const [ alertShown, setAlertShown ] = useState(false);
+    const [ errorMsg, setErrorMsg ] = useState('');
 
     // effect, we only need to load the posts once
     useEffect(() => {
@@ -65,6 +80,24 @@ const PostList = () => {
         );
     });
 
+    // event listener
+    window.onscroll = function(e) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            // the user has reached the bottom of the page (and read all posts)
+            setCurrentPageOfPosts(currentPageOfPosts + 1);
+            API.get(`posts?page=${currentPageOfPosts}&per_page=${NUMBER_OF_POST_TO_LOAD}`).then(
+                res => {
+                    // we fetch and append more posts
+                    setPostList(postList.concat(res.data));
+                },
+                err => {
+                    // we do not want to bother the user with these errors
+                    console.log("" + err); 
+                }
+            );
+        }
+    };
+
     return (
         <div>
             <Snackbar open={alertShown} autoHideDuration={5000} onClose={() => setAlertShown(false)}>
@@ -72,9 +105,9 @@ const PostList = () => {
             </Snackbar>
 
             { isLoading ?
-                <CircularProgress color="secondary" />
+                <span className={classes.center} ><CircularProgress color="secondary" /></span>
             : errorFromServer ?
-                <p>There was an error</p>
+                <p className={classes.center}>{errorMsg}</p>
             :
                 <List>
                     { postList.map( post => <PostListItem post={post} key={post._id} />) }
